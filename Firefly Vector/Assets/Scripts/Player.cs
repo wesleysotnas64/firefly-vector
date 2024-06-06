95dda1878a0a58ada75e05427e9d9badc75be129
Wesley Santos <wesleysotnas64@gmail.com>
2024-06-06 14:05:43 -0300

    feat: Poste de luz e atração do vagalume.

--- Firefly Vector/Assets/Scripts/Player.cs
+++ Firefly Vector/Assets/Scripts/Player.cs
@@ -1,17 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]
     private float speed;
+
     [SerializeField]
     private bool ableToMove;
+
+    [SerializeField]
+    private Vector2 attractionObjectPosition;
+
+    [SerializeField]
+    private bool inAttractionArea;
+
+    [SerializeField]
+    private Vector2 attractVector;
+
+    [SerializeField]
+    private float attractSpeed;
 
     void Start()
     {
         ableToMove = false;
+        inAttractionArea = false;
+        attractVector = transform.up;
+        attractSpeed = 1.0f;
     }
 
     void Update()
@@ -19,6 +36,20 @@ public class Player : MonoBehaviour
         if (ableToMove)
         {
             Move();
+        }
+
+        if (inAttractionArea)
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+            attractVector = (attractionObjectPosition - (Vector2) transform.position).normalized;
+
+            Vector2 newDirection = Vector2.Lerp(transform.up, attractVector, Time.deltaTime * attractSpeed);
+
+            SetDirection(newDirection);
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = Color.white;
         }
     }
     
@@ -38,27 +69,6 @@ public class Player : MonoBehaviour
         ableToMove = true;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        string tag = collision.gameObject.tag;
-
-        Vector2 newDirection = Vector2.zero;
-
-        if (tag == "Obstacle")
-        {
-            newDirection = ReflecVector(transform.up, collision.gameObject.transform.up);
-            Destroy(collision.gameObject);
-        }
-        else if (tag == "CircleObstacle")
-        {
-            newDirection = ReflecVector(transform.up, (Vector2) (collision.gameObject.transform.position - transform.position).normalized);
-            Destroy(collision.gameObject);
-        }
-
-        SetDirection(newDirection);
-        
-    }
-
     private Vector2 ReflecVector(Vector2 input, Vector2 normal)
     {
         input.Normalize();
@@ -68,4 +78,47 @@ public class Player : MonoBehaviour
 
         return reflection;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        string tag = collision.gameObject.tag;
+
+        if (tag == "Obstacle")
+        {
+            Vector2 newDirection = ReflecVector(transform.up, collision.gameObject.transform.up);
+            SetDirection(newDirection);
+            Destroy(collision.gameObject);
+        }
+        else if (tag == "CircleObstacle")
+        {
+            Vector2 newDirection = ReflecVector(transform.up, (Vector2) (collision.gameObject.transform.position - transform.position).normalized);
+            SetDirection(newDirection);
+            Destroy(collision.gameObject);
+        }
+        else if (tag == "IndestructibleObstacle")
+        {
+            Vector2 newDirection = ReflecVector(transform.up, collision.gameObject.transform.up);
+            SetDirection(newDirection);
+        }
+        else if (tag == "IndestructibleCircleObstacle")
+        {
+            Vector2 newDirection = ReflecVector(transform.up, (Vector2) (collision.gameObject.transform.position - transform.position).normalized);
+            SetDirection(newDirection);
+        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        inAttractionArea = true;
+        attractionObjectPosition = (Vector2) col.gameObject.transform.position;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        inAttractionArea = false;
+        attractVector = transform.up;
+    }
+
+    
 }
